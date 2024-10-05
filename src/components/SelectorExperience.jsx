b0bfc5198395644a1a8c1d8f568365f121170cd1
Daniel-rod99 <danielrodriguez246@gmail.com>
2024-10-06 00:18:25 +0200

    Actualización del proyecto

--- src/components/SelectorExperience.jsx
+++ src/components/SelectorExperience.jsx
@@ -1,20 +1,78 @@
-import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Input,
+  Button,
+  Chip,
+} from "@nextui-org/react";
 import { works } from "../data/works";
+import { useState } from "react";
 
 export default function SelectorExperience() {
+  const [selectedValue, setSelectedValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [experienceItem, setExperienceItem] = useState([]);
+
+  const handleSelect = (value) => {
+    setSelectedValue(value);
+  };
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddExperience = () => {
+    if (inputValue.trim() || selectedValue.trim() !== "") {
+      setExperienceItem([
+        ...experienceItem,
+        { role: inputValue, work: selectedValue },
+      ]);
+      setInputValue("");
+      setSelectedValue("");
+    }
+  };
+
   return (
-    <div className="flex flex-col w-full max-w-xs gap-2">
-      <Autocomplete
-        allowsCustomValue
-        label="Search works"
-        variant="bordered"
-        className="max-w-xs"
-        defaultItems={works}
-      >
-        {(item) => (
-          <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>
-        )}
-      </Autocomplete>
+    <div className="flex flex-col gap-3">
+      <div>
+        <ul className="flex gap-3">
+          {experienceItem.map((item, index) => (
+            <li key={index} className="flex items-center gap-3">
+              <Chip>{item.work}</Chip>
+              <p className="">{item.role}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col gap-3 lg:flex-row">
+        <Autocomplete
+          defaultItems={works}
+          allowsCustomValue
+          label="Search works"
+          variant="bordered"
+          className="max-w-auto"
+          value={selectedValue}
+          onSelectionChange={handleSelect}
+        >
+          {(item) => (
+            <AutocompleteItem key={item.label}>{item.label}</AutocompleteItem>
+          )}
+        </Autocomplete>
+        <Input
+          type="text"
+          label="Describe your role"
+          className="max-w-auto"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <Button
+          color="primary"
+          size="lg"
+          className="lg:w-[20rem]"
+          onClick={handleAddExperience}
+        >
+          + Add Experience
+        </Button>
+      </div>
     </div>
   );
 }
--- src/data/works.js
+++ src/data/works.js
@@ -1,451 +1,311 @@
 export const works = [
   {
-    label: "Software Engineer",
-    value: "software_engineer",
-  },
-  {
-    label: "Doctor",
-    value: "doctor",
-  },
-  {
-    label: "Nurse",
-    value: "nurse",
-  },
-  {
-    label: "Teacher",
-    value: "teacher",
-  },
-  {
-    label: "Driver",
-    value: "driver",
-  },
-  {
-    label: "Data Scientist",
-    value: "data_scientist",
-  },
-  {
     label: "Accountant",
     value: "accountant",
   },
   {
-    label: "Graphic Designer",
-    value: "graphic_designer",
-  },
-  {
-    label: "Marketing Manager",
-    value: "marketing_manager",
-  },
-  {
-    label: "Civil Engineer",
-    value: "civil_engineer",
-  },
-  {
-    label: "Lawyer",
-    value: "lawyer",
-  },
-  {
-    label: "Pharmacist",
-    value: "pharmacist",
-  },
-  {
-    label: "Construction Worker",
-    value: "construction_worker",
-  },
-  {
-    label: "Chef",
-    value: "chef",
-  },
-  {
-    label: "Sales Representative",
-    value: "sales_representative",
-  },
-  {
-    label: "Architect",
-    value: "architect",
-  },
-  {
-    label: "Electrician",
-    value: "electrician",
-  },
-  {
-    label: "Plumber",
-    value: "plumber",
-  },
-  {
-    label: "Dentist",
-    value: "dentist",
-  },
-  {
-    label: "Mechanic",
-    value: "mechanic",
-  },
-  {
-    label: "Pilot",
-    value: "pilot",
-  },
-  {
-    label: "Journalist",
-    value: "journalist",
-  },
-  {
-    label: "Event Planner",
-    value: "event_planner",
-  },
-  {
-    label: "Fitness Trainer",
-    value: "fitness_trainer",
-  },
-  {
-    label: "Barber",
-    value: "barber",
-  },
-  {
-    label: "Hairdresser",
-    value: "hairdresser",
-  },
-  {
-    label: "Real Estate Agent",
-    value: "real_estate_agent",
-  },
-  {
-    label: "Psychologist",
-    value: "psychologist",
-  },
-  {
-    label: "Social Worker",
-    value: "social_worker",
-  },
-  {
-    label: "Veterinarian",
-    value: "veterinarian",
-  },
-  {
-    label: "Translator",
-    value: "translator",
-  },
-  {
-    label: "Customer Service Representative",
-    value: "customer_service_representative",
-  },
-  {
-    label: "Business Analyst",
-    value: "business_analyst",
-  },
-  {
-    label: "Financial Analyst",
-    value: "financial_analyst",
-  },
-  {
-    label: "Project Manager",
-    value: "project_manager",
-  },
-  {
-    label: "HR Manager",
-    value: "hr_manager",
-  },
-  {
-    label: "Web Developer",
-    value: "web_developer",
-  },
-  {
-    label: "Cybersecurity Specialist",
-    value: "cybersecurity_specialist",
-  },
-  {
-    label: "SEO Specialist",
-    value: "seo_specialist",
-  },
-  {
-    label: "Phlebotomist",
-    value: "phlebotomist",
-  },
-  {
-    label: "UX/UI Designer",
-    value: "ux_ui_designer",
-  },
-  {
-    label: "Operations Manager",
-    value: "operations_manager",
-  },
-  {
-    label: "Physiotherapist",
-    value: "physiotherapist",
-  },
-  {
-    label: "Flight Attendant",
-    value: "flight_attendant",
-  },
-  {
-    label: "Security Guard",
-    value: "security_guard",
-  },
-  {
-    label: "Scientist",
-    value: "scientist",
-  },
-  {
-    label: "Chef de Cuisine",
-    value: "chef_de_cuisine",
-  },
-  {
-    label: "Software Tester",
-    value: "software_tester",
-  },
-  {
-    label: "Entrepreneur",
-    value: "entrepreneur",
-  },
-  {
-    label: "Animator",
-    value: "animator",
-  },
-  {
-    label: "Bartender",
-    value: "bartender",
-  },
-  {
-    label: "Receptionist",
-    value: "receptionist",
-  },
-  {
-    label: "Photographer",
-    value: "photographer",
-  },
-  {
-    label: "Film Director",
-    value: "film_director",
-  },
-  {
-    label: "Radio Broadcaster",
-    value: "radio_broadcaster",
-  },
-  {
-    label: "Content Writer",
-    value: "content_writer",
-  },
-  {
-    label: "Tour Guide",
-    value: "tour_guide",
-  },
-  {
-    label: "Bank Teller",
-    value: "bank_teller",
-  },
-  {
-    label: "Software Architect",
-    value: "software_architect",
-  },
-  {
-    label: "Truck Driver",
-    value: "truck_driver",
-  },
-  {
-    label: "Paramedic",
-    value: "paramedic",
-  },
-  {
-    label: "Landscape Architect",
-    value: "landscape_architect",
-  },
-  {
-    label: "Surveyor",
-    value: "surveyor",
-  },
-  {
-    label: "Tax Advisor",
-    value: "tax_advisor",
-  },
-  {
-    label: "Courier",
-    value: "courier",
-  },
-  {
-    label: "Customs Officer",
-    value: "customs_officer",
-  },
-  {
-    label: "IT Support Specialist",
-    value: "it_support_specialist",
-  },
-  {
-    label: "Logistics Coordinator",
-    value: "logistics_coordinator",
-  },
-  {
-    label: "Mechanic Engineer",
-    value: "mechanic_engineer",
-  },
-  {
-    label: "Clinical Researcher",
-    value: "clinical_researcher",
-  },
-  {
-    label: "Store Manager",
-    value: "store_manager",
+    label: "Administrative Assistant",
+    value: "administrative_assistant",
   },
   {
     label: "Advertising Manager",
     value: "advertising_manager",
   },
   {
-    label: "Biologist",
-    value: "biologist",
-  },
-  {
-    label: "Environmental Engineer",
-    value: "environmental_engineer",
-  },
-  {
-    label: "Plumber",
-    value: "plumber",
+    label: "Agricultural Engineer",
+    value: "agricultural_engineer",
   },
   {
     label: "Air Traffic Controller",
     value: "air_traffic_controller",
   },
   {
-    label: "Chef",
-    value: "chef",
+    label: "Animator",
+    value: "animator",
+  },
+  {
+    label: "Anesthesiologist",
+    value: "anesthesiologist",
+  },
+  {
+    label: "Archaeologist",
+    value: "archaeologist",
+  },
+  {
+    label: "Architect",
+    value: "architect",
+  },
+  {
+    label: "Art Director",
+    value: "art_director",
+  },
+  {
+    label: "Astrologer",
+    value: "astrologer",
+  },
+  {
+    label: "Astronomer",
+    value: "astronomer",
+  },
+  {
+    label: "Automotive Technician",
+    value: "automotive_technician",
+  },
+  {
+    label: "Baker",
+    value: "baker",
+  },
+  {
+    label: "Barber",
+    value: "barber",
+  },
+  {
+    label: "Bartender",
+    value: "bartender",
+  },
+  {
+    label: "Barista",
+    value: "barista",
+  },
+  {
+    label: "Barge Operator",
+    value: "barge_operator",
+  },
+  {
+    label: "Biologist",
+    value: "biologist",
   },
   {
     label: "Biomedical Engineer",
     value: "biomedical_engineer",
   },
   {
-    label: "Warehouse Worker",
-    value: "warehouse_worker",
+    label: "Blogger",
+    value: "blogger",
   },
   {
-    label: "Travel Agent",
-    value: "travel_agent",
+    label: "Business Analyst",
+    value: "business_analyst",
   },
   {
-    label: "Massage Therapist",
-    value: "massage_therapist",
+    label: "Business Development Manager",
+    value: "business_development_manager",
   },
   {
-    label: "Dietitian",
-    value: "dietitian",
+    label: "Carpenter",
+    value: "carpenter",
   },
   {
-    label: "Nutritionist",
-    value: "nutritionist",
+    label: "Chef",
+    value: "chef",
   },
   {
-    label: "Personal Trainer",
-    value: "personal_trainer",
+    label: "Childcare Provider",
+    value: "childcare_provider",
   },
   {
-    label: "Legal Assistant",
-    value: "legal_assistant",
+    label: "Chiropractor",
+    value: "chiropractor",
   },
   {
-    label: "Pharmacy Technician",
-    value: "pharmacy_technician",
+    label: "Civil Engineer",
+    value: "civil_engineer",
   },
   {
-    label: "Bank Manager",
-    value: "bank_manager",
+    label: "Clinical Researcher",
+    value: "clinical_researcher",
   },
   {
-    label: "Systems Analyst",
-    value: "systems_analyst",
+    label: "Comedian",
+    value: "comedian",
   },
   {
-    label: "PR Specialist",
-    value: "pr_specialist",
+    label: "Compliance Officer",
+    value: "compliance_officer",
   },
   {
-    label: "Financial Planner",
-    value: "financial_planner",
+    label: "Construction Worker",
+    value: "construction_worker",
   },
   {
-    label: "Stock Trader",
-    value: "stock_trader",
+    label: "Consultant",
+    value: "consultant",
   },
   {
-    label: "Interior Designer",
-    value: "interior_designer",
+    label: "Content Creator",
+    value: "content_creator",
   },
   {
-    label: "Event Coordinator",
-    value: "event_coordinator",
-  },
-  {
-    label: "Criminal Investigator",
-    value: "criminal_investigator",
-  },
-  {
-    label: "Museum Curator",
-    value: "museum_curator",
-  },
-  {
-    label: "Game Developer",
-    value: "game_developer",
-  },
-  {
-    label: "Digital Marketing Manager",
-    value: "digital_marketing_manager",
-  },
-  {
-    label: "Research Scientist",
-    value: "research_scientist",
-  },
-  {
-    label: "Microbiologist",
-    value: "microbiologist",
-  },
-  {
-    label: "Electrical Engineer",
-    value: "electrical_engineer",
-  },
-  {
-    label: "Chemical Engineer",
-    value: "chemical_engineer",
-  },
-  {
-    label: "Statistician",
-    value: "statistician",
-  },
-  {
-    label: "Zoologist",
-    value: "zoologist",
-  },
-  {
-    label: "Agricultural Engineer",
-    value: "agricultural_engineer",
-  },
-  {
-    label: "Firefighter",
-    value: "firefighter",
-  },
-  {
-    label: "Police Officer",
-    value: "police_officer",
-  },
-  {
-    label: "Kindergarten Teacher",
-    value: "kindergarten_teacher",
-  },
-  {
-    label: "Video Editor",
-    value: "video_editor",
+    label: "Content Writer",
+    value: "content_writer",
   },
   {
     label: "Copywriter",
     value: "copywriter",
   },
   {
-    label: "Public Relations Manager",
-    value: "public_relations_manager",
+    label: "Courier",
+    value: "courier",
+  },
+  {
+    label: "Creative Director",
+    value: "creative_director",
+  },
+  {
+    label: "Criminal Investigator",
+    value: "criminal_investigator",
+  },
+  {
+    label: "Customer Service Representative",
+    value: "customer_service_representative",
+  },
+  {
+    label: "Cybersecurity Specialist",
+    value: "cybersecurity_specialist",
+  },
+  {
+    label: "Data Analyst",
+    value: "data_analyst",
+  },
+  {
+    label: "Data Scientist",
+    value: "data_scientist",
+  },
+  {
+    label: "Delivery Driver",
+    value: "delivery_driver",
+  },
+  {
+    label: "Dentist",
+    value: "dentist",
+  },
+  {
+    label: "Dietitian",
+    value: "dietitian",
+  },
+  {
+    label: "Digital Marketing Manager",
+    value: "digital_marketing_manager",
+  },
+  {
+    label: "Digital Marketing Specialist",
+    value: "digital_marketing_specialist",
+  },
+  {
+    label: "Director of Photography",
+    value: "director_of_photography",
+  },
+  {
+    label: "Distribution Center Manager",
+    value: "distribution_center_manager",
+  },
+  {
+    label: "Driver",
+    value: "driver",
+  },
+  {
+    label: "Drone Operator",
+    value: "drone_operator",
   },
   {
     label: "Economist",
     value: "economist",
   },
   {
-    label: "Compliance Officer",
-    value: "compliance_officer",
+    label: "Electrician",
+    value: "electrician",
+  },
+  {
+    label: "Electrical Engineer",
+    value: "electrical_engineer",
+  },
+  {
+    label: "Emergency Medical Technician (EMT)",
+    value: "emt",
+  },
+  {
+    label: "Entrepreneur",
+    value: "entrepreneur",
+  },
+  {
+    label: "Environmental Engineer",
+    value: "environmental_engineer",
+  },
+  {
+    label: "Environmental Scientist",
+    value: "environmental_scientist",
+  },
+  {
+    label: "Event Coordinator",
+    value: "event_coordinator",
+  },
+  {
+    label: "Event Planner",
+    value: "event_planner",
+  },
+  {
+    label: "Film Director",
+    value: "film_director",
+  },
+  {
+    label: "Financial Analyst",
+    value: "financial_analyst",
+  },
+  {
+    label: "Financial Planner",
+    value: "financial_planner",
+  },
+  {
+    label: "Fitness Instructor",
+    value: "fitness_instructor",
+  },
+  {
+    label: "Fitness Trainer",
+    value: "fitness_trainer",
+  },
+  {
+    label: "Freelance Writer",
+    value: "freelance_writer",
+  },
+  {
+    label: "Freight Broker",
+    value: "freight_broker",
+  },
+  {
+    label: "Game Developer",
+    value: "game_developer",
+  },
+  {
+    label: "Geologist",
+    value: "geologist",
+  },
+  {
+    label: "Graphic Designer",
+    value: "graphic_designer",
+  },
+  {
+    label: "Hairdresser",
+    value: "hairdresser",
+  },
+  {
+    label: "HR Manager",
+    value: "hr_manager",
+  },
+  {
+    label: "Housekeeper",
+    value: "housekeeper",
+  },
+  {
+    label: "Hotel Manager",
+    value: "hotel_manager",
+  },
+  {
+    label: "HVAC Technician",
+    value: "hvac_technician",
+  },
+  {
+    label: "Illustrator",
+    value: "illustrator",
   },
   {
     label: "Insurance Agent",
@@ -456,140 +316,271 @@ export const works = [
     value: "insurance_underwriter",
   },
   {
-    label: "Fitness Instructor",
-    value: "fitness_instructor",
+    label: "Interior Designer",
+    value: "interior_designer",
   },
   {
-    label: "Software Developer",
-    value: "software_developer",
+    label: "IT Support Specialist",
+    value: "it_support_specialist",
   },
   {
-    label: "Drone Operator",
-    value: "drone_operator",
+    label: "Journalist",
+    value: "journalist",
   },
   {
-    label: "Art Director",
-    value: "art_director",
+    label: "Kindergarten Teacher",
+    value: "kindergarten_teacher",
   },
   {
-    label: "Music Producer",
-    value: "music_producer",
-  },
-  {
-    label: "Carpenter",
-    value: "carpenter",
-  },
-  {
-    label: "Welder",
-    value: "welder",
-  },
-  {
-    label: "Baker",
-    value: "baker",
-  },
-  {
-    label: "Florist",
-    value: "florist",
-  },
-  {
-    label: "Painter",
-    value: "painter",
-  },
-  {
-    label: "Butcher",
-    value: "butcher",
-  },
-  {
-    label: "Tailor",
-    value: "tailor",
-  },
-  {
-    label: "Hotel Manager",
-    value: "hotel_manager",
-  },
-  {
-    label: "Chiropractor",
-    value: "chiropractor",
-  },
-  {
-    label: "Geologist",
-    value: "geologist",
-  },
-  {
-    label: "Pet Groomer",
-    value: "pet_groomer",
-  },
-  {
-    label: "Park Ranger",
-    value: "park_ranger",
-  },
-  {
-    label: "Archaeologist",
-    value: "archaeologist",
-  },
-  {
-    label: "Astrologer",
-    value: "astrologer",
-  },
-  {
-    label: "Astronomer",
-    value: "astronomer",
-  },
-
-  {
-    label: "Sommelier",
-    value: "sommelier",
-  },
-  {
-    label: "Chef",
-    value: "chef",
-  },
-  {
-    label: "Comedian",
-    value: "comedian",
-  },
-  {
-    label: "Magician",
-    value: "magician",
-  },
-  {
-    label: "Makeup Artist",
-    value: "makeup_artist",
-  },
-  {
-    label: "Dancer",
-    value: "dancer",
-  },
-  {
-    label: "Sound Engineer",
-    value: "sound_engineer",
+    label: "Legal Assistant",
+    value: "legal_assistant",
   },
   {
     label: "Lighting Technician",
     value: "lighting_technician",
   },
   {
-    label: "Set Designer",
-    value: "set_designer",
+    label: "Logistics Coordinator",
+    value: "logistics_coordinator",
   },
   {
-    label: "Producer",
-    value: "producer",
+    label: "Magician",
+    value: "magician",
   },
   {
-    label: "Director of Photography",
-    value: "director_of_photography",
+    label: "Marketing Consultant",
+    value: "marketing_consultant",
   },
   {
-    label: "Talent Agent",
-    value: "talent_agent",
+    label: "Marketing Manager",
+    value: "marketing_manager",
   },
   {
-    label: "Animator",
-    value: "animator",
+    label: "Marketing Specialist",
+    value: "marketing_specialist",
   },
   {
-    label: "Voice Actor",
-    value: "voice_actor",
+    label: "Market Research Analyst",
+    value: "market_research_analyst",
+  },
+  {
+    label: "Medical Assistant",
+    value: "medical_assistant",
+  },
+  {
+    label: "Mechanic",
+    value: "mechanic",
+  },
+  {
+    label: "Mechanic Engineer",
+    value: "mechanic_engineer",
+  },
+  {
+    label: "Microbiologist",
+    value: "microbiologist",
+  },
+  {
+    label: "Museum Curator",
+    value: "museum_curator",
+  },
+  {
+    label: "Music Producer",
+    value: "music_producer",
+  },
+  {
+    label: "Nurse",
+    value: "nurse",
+  },
+  {
+    label: "Nutritionist",
+    value: "nutritionist",
+  },
+  {
+    label: "Occupational Therapist",
+    value: "occupational_therapist",
+  },
+  {
+    label: "Operations Manager",
+    value: "operations_manager",
+  },
+  {
+    label: "Painter",
+    value: "painter",
+  },
+  {
+    label: "Paramedic",
+    value: "paramedic",
+  },
+  {
+    label: "Park Ranger",
+    value: "park_ranger",
+  },
+  {
+    label: "Personal Trainer",
+    value: "personal_trainer",
+  },
+  {
+    label: "Broker",
+    value: "broker",
+  },
+  {
+    label: "Butcher",
+    value: "butcher",
+  },
+  {
+    label: "Chemical Engineer",
+    value: "chemical_engineer",
+  },
+  {
+    label: "Customs Officer",
+    value: "customs_officer",
+  },
+  {
+    label: "Doctor",
+    value: "doctor",
+  },
+  {
+    label: "Economic Analyst",
+    value: "economic_analyst",
+  },
+  {
+    label: "Florist",
+    value: "florist",
+  },
+  {
+    label: "Freelance Graphic Designer",
+    value: "freelance_graphic_designer",
+  },
+  {
+    label: "Pet Groomer",
+    value: "pet_groomer",
+  },
+  {
+    label: "Pharmacist",
+    value: "pharmacist",
+  },
+  {
+    label: "Pharmacy Technician",
+    value: "pharmacy_technician",
+  },
+  {
+    label: "Phlebotomist",
+    value: "phlebotomist",
+  },
+  {
+    label: "Photographer",
+    value: "photographer",
+  },
+  {
+    label: "Physical Therapist",
+    value: "physical_therapist",
+  },
+  {
+    label: "Physician",
+    value: "physician",
+  },
+  {
+    label: "Plumber",
+    value: "plumber",
+  },
+  {
+    label: "Police Officer",
+    value: "police_officer",
+  },
+  {
+    label: "Production Assistant",
+    value: "production_assistant",
+  },
+  {
+    label: "Project Manager",
+    value: "project_manager",
+  },
+  {
+    label: "Psychologist",
+    value: "psychologist",
+  },
+  {
+    label: "Public Relations Specialist",
+    value: "public_relations_specialist",
+  },
+  {
+    label: "Quality Assurance Analyst",
+    value: "quality_assurance_analyst",
+  },
+  {
+    label: "Real Estate Agent",
+    value: "real_estate_agent",
+  },
+  {
+    label: "Recruiter",
+    value: "recruiter",
+  },
+  {
+    label: "Research Scientist",
+    value: "research_scientist",
+  },
+  {
+    label: "Retail Manager",
+    value: "retail_manager",
+  },
+  {
+    label: "Sales Associate",
+    value: "sales_associate",
+  },
+  {
+    label: "Sales Manager",
+    value: "sales_manager",
+  },
+  {
+    label: "Social Media Manager",
+    value: "social_media_manager",
+  },
+  {
+    label: "Software Developer",
+    value: "software_developer",
+  },
+  {
+    label: "Software Engineer",
+    value: "software_engineer",
+  },
+  {
+    label: "Statistician",
+    value: "statistician",
+  },
+  {
+    label: "Surgeon",
+    value: "surgeon",
+  },
+  {
+    label: "Teacher",
+    value: "teacher",
+  },
+  {
+    label: "Technical Writer",
+    value: "technical_writer",
+  },
+  {
+    label: "Telecommunications Specialist",
+    value: "telecommunications_specialist",
+  },
+  {
+    label: "Web Developer",
+    value: "web_developer",
+  },
+  {
+    label: "Web Designer",
+    value: "web_designer",
+  },
+  {
+    label: "Wedding Planner",
+    value: "wedding_planner",
+  },
+  {
+    label: "Writer",
+    value: "writer",
+  },
+  {
+    label: "Zoologist",
+    value: "zoologist",
   },
 ];
